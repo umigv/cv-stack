244ab11dc999bdadf6a82ff0dddc4da85b353409
jmcfee <jmcfee@umich.edu>
2024-03-10 17:13:54 -0400

    Updated drivable_area, added a test publisher to mimic ZED video, drivable_area.py should take ZED video and turn it into occupancy grid, then publish occupancy grid

--- install/setup.sh
+++ install/setup.sh
@@ -7,7 +7,7 @@
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/umarv/arvws/cv-nav-integration/install
+_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/jmcfee/arv/cv-nav-integration/install
 if [ ! -z "$COLCON_CURRENT_PREFIX" ]; then
   _colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX="$COLCON_CURRENT_PREFIX"
 elif [ ! -d "$_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX" ]; then
---
+++ src/drivable_area/drivable_area/drivable_area_test_publisher.py
@@ -0,0 +1,63 @@
+import cv2
+import numpy as np
+from sensor_msgs.msg import Image
+import rclpy
+from rclpy.node import Node
+from cv_bridge import CvBridge
+
+
+video = cv2.VideoCapture("src/drivable_area/drivable_area/utils/comp23_2.mp4")
+
+
+fps = int(video.get(cv2.CAP_PROP_FPS))
+width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output_video.avi', fourcc, fps, (width, height), isColor=True)
+out2 = cv2.VideoWriter('occupancy_grid.avi', fourcc, fps, (width, height), isColor=True)
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        # Call the constructor of the parent class Node
+        super().__init__('minimal_publisher')
+        
+        self.bridge = CvBridge()
+        # Create a publisher that publishes OccupancyGrid messages on the 'topic' topic
+        self.publisher_ = self.create_publisher(Image, 'url', 10)
+        # Create a timer that calls the timer_callback method every 0.5 seconds
+        timer_period = 1 / fps  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        
+        ret, frame = video.read()
+        if not ret:
+            return
+        # Publish the occupancy grid
+        ros_image = self.bridge.cv2_to_imgmsg(frame, encoding="bgr8")
+        self.publisher_.publish(ros_image)
+        # Log a message
+        self.get_logger().info('Publishing ZED frame')
+        
+        
+        
+# The main function
+def main(args=None):
+    # Initialize rclpy
+    rclpy.init(args=args)
+
+    # Create a MinimalPublisher object
+    minimal_publisher = MinimalPublisher()
+
+    # Spin the node so it can process callbacks
+    rclpy.spin(minimal_publisher)
+
+    # After spinning, destroy the node and shutdown rclpy
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+# If this script is run directly, call the main function
+if __name__ == '__main__':
+    main()
