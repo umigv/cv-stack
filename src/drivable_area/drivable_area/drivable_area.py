d010e1068631c7efa342c9dfeb765bdca97ea9e9
Kanishk Kandoi <kanishkkandoi52@gmail.com>
2024-03-10 15:15:34 -0400

    Fixes

--- src/drivable_area/drivable_area/drivable_area.py
+++ src/drivable_area/drivable_area/drivable_area.py
@@ -6,6 +6,8 @@ import cv2
 import numpy as np
 import CameraProperties from bev
 from ultralytics import YOLO
+from nav_msgs.msg import OccupancyGrid, MapMetaData
+from array import array as Array
 
 model = YOLO('./utils/nov13.pt')
 
@@ -34,6 +36,7 @@ class DrivableArea(Node):
             instance_mask = np.ones((frame.shape[0], frame.shape[1]))
             cv2.fillPoly(instance_mask, [mask], 0)
             grid = np.logical_or(grid, instance_mask)
+
         occupancy_grid_display = grid.astype(np.uint8) * 255
         transformed_image = getBirdView(occupancy_grid_display, ZED)
         current_pixel_size = 0.006  # current size each pixel represents in meters
@@ -44,11 +47,12 @@ class DrivableArea(Node):
         resized_image = cv2.resize(transformed_image, new_size, interpolation = cv2.INTER_AREA)
 
         rob_arr = np.full((20, 161), -1, dtype=np.uint8)
+        
         rob_arr[10][80] = 2
 
         combined_arr = np.concatenate((resized_image, rob_arr), axis=0)
-
-
+        grid_to_publish = numpy_to_occupancy_grid(combined_arr)
+        # print(grid_to_publish)
 
 def main(args=None):
     rclpy.init(args=args)
@@ -63,6 +67,23 @@ def main(args=None):
     node.destroy_node()
     rclpy.shutdown()
 
+def numpy_to_occupancy_grid(arr, info=None, x=80,y=77):
+    if not len(arr.shape) == 2:
+        raise TypeError('Array must be 2D')
+    if not arr.dtype == np.int8:
+        raise TypeError('Array must be of int8s')
 
+    grid = OccupancyGrid()
+    if isinstance(arr, np.ma.MaskedArray):
+        # We assume that the masked value are already -1, for speed
+        arr = arr.data
+
+    grid.data = Array('b', arr.ravel().astype(np.int8))
+    grid.info = info or MapMetaData()
+    grid.info.height = arr.shape[0]
+    grid.info.width = arr.shape[1]
+    grid.info.geometry_msgs/Pose origin = (x,y,0)
+
+    return grid
 if __name__ == '__main__':
     main()
