16a8bec656666a39aa0b5d6c1b2f790f348008cb
Arnav Shah <sarn248@gmail.com>
2024-03-17 16:05:20 -0400

    Add Comments

--- src/drivable_area/drivable_area/drivable_area.py
+++ src/drivable_area/drivable_area/drivable_area.py
@@ -1,5 +1,3 @@
-# THIS WILL SUBSCRIBE TO ZED CAMERA AND PRODUCE AN OCCUPANCY GRID
-
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
@@ -10,99 +8,15 @@ from ultralytics import YOLO
 from nav_msgs.msg import OccupancyGrid, MapMetaData
 from array import array as Array
 from std_msgs.msg import Header
-from math import radians, cos
 import math
 import time
+from drivable_area.bev import CameraProperties, getBirdView
 
-
-
+# Load the YOLO models for lane and pothole detection
 lane_model = YOLO('drivable_area/drivable_area/utils/LLOnly180ep.pt')
 hole_model = YOLO('drivable_area/drivable_area/utils/potholesonly100epochs.pt')
 
-class CameraProperties(object):
-    functional_limit = radians(70.0)
-    def __init__(self, height, fov_vert, fov_horz, cameraTilt):
-        self.height = float(height)
-        self.fov_vert = radians(float(fov_vert))
-        self.fov_horz = radians(float(fov_horz))
-        self.cameraTilt = radians(float(cameraTilt))
-        self.bird_src_quad = None
-        self.bird_dst_quad = None
-        self.matrix = None
-        self.maxHeight = None
-        self.maxWidth = None
-        self.minIndex = None
-
-    def src_quad(self, rows, columns):
-        if self.bird_src_quad is None:
-            self.bird_src_quad = np.array([[0, rows - 1], [columns - 1, rows - 1], [0, 0], [columns - 1, 0]], dtype = 'float32')
-        return self.bird_src_quad
-
-    def dst_quad(self, rows, columns, min_angle, max_angle):
-        if self.bird_dst_quad is None:
-            fov_offset = self.cameraTilt - self.fov_vert/2.0
-            bottom_over_top = cos(max_angle + fov_offset)/cos(min_angle + fov_offset)
-            bottom_width = columns*bottom_over_top
-            blackEdge_width = (columns - bottom_width)/2
-            leftX = blackEdge_width
-            rightX = leftX + bottom_width
-            self.bird_dst_quad = np.array([[leftX, rows], [rightX, rows], [0, 0], [columns, 0]], dtype = 'float32')
-        return self.bird_dst_quad
-
-    def reset(self):
-        self.bird_src_quad = None
-        self.bird_dst_quad = None
-        self.matrix = None
-        self.maxHeight = None
-        self.maxWidth = None
-        self.minIndex = None
-
-    def compute_min_index(self, rows, max_angle):
-        self.minIndex = int(rows*(1.0 - max_angle/self.fov_vert))
-        return self.minIndex
-
-    def compute_max_angle(self):
-        return min(CameraProperties.functional_limit - self.cameraTilt + self.fov_vert/2.0, self.fov_vert)
-
-def getBirdView(image, cp):
-    rows, columns = image.shape[:2]
-    print(rows, columns)
-    if columns == 1280:
-        columns = 1344
-    if rows == 720:
-        rows = 752
-    min_angle = 0.0
-    max_angle = cp.compute_max_angle()
-    min_index = cp.compute_min_index(rows, max_angle)
-    image = image[min_index:, :]
-    rows = image.shape[0]
-
-    src_quad = cp.src_quad(rows, columns)
-    dst_quad = cp.dst_quad(rows, columns, min_angle, max_angle)
-    warped, bottomLeft, bottomRight, topRight, topLeft = perspective(image, src_quad, dst_quad, cp)
-    return warped, bottomLeft, bottomRight, topRight, topLeft, cp.maxWidth, cp.maxHeight
-
-def perspective(image, src_quad, dst_quad, cp):
-    bottomLeft, bottomRight, topLeft, topRight = dst_quad
-    widthA = topRight[0] - topLeft[0]
-    widthB = bottomRight[0] - bottomLeft[0]
-    maxWidth1 = max(widthA, widthB)
-    heightA = bottomLeft[1] - topLeft[1]
-    heightB = bottomRight[1] - topRight[1]
-    maxHeight1 = max(heightA, heightB)
-
-    matrix1 = cv2.getPerspectiveTransform(src_quad, dst_quad)
-    cp.matrix = matrix1
-    cp.maxWidth = int(maxWidth1)
-    cp.maxHeight = int(maxHeight1)
-
-    warped = cv2.warpPerspective(image, matrix1, (cp.maxWidth, cp.maxHeight))
-
-
-    return warped, bottomLeft, bottomRight, topRight, topLeft
-
 class DrivableArea(Node):
-
     def __init__(self):
         super().__init__('drivable_area')
         
@@ -119,17 +33,22 @@ class DrivableArea(Node):
         self.desired_size = 0.05
         self.scale_factor = self.curr_pix_size / self.desired_size
 
+        # Create a publisher that publishes OccupancyGrid messages on the 'occupancy_grid' topic
         self.publisher = self.create_publisher(OccupancyGrid, 'occupancy_grid', 10)
 
     def get_occupancy_grid(self, frame):
         
+        # Predict the lane
         r_lane = lane_model.predict(frame, conf=0.5)[0]
-        # lane_annotated_frame = r_lane.plot()
         image_width, image_height = frame.shape[1], frame.shape[0]
         
+        # Create an empty occupancy grid
         occupancy_grid = np.zeros((image_height, image_width))
+
+        # Predict the potholes
         r_hole = hole_model.predict(frame, conf=0.25)[0]
 
+        # If the lane is detected, fill the occupancy grid with the lane and mark the undrivable area as occupied
         time_of_frame = 0
         if r_lane.masks is not None:
             if(len(r_lane.masks.xy) != 0):
@@ -138,6 +57,7 @@ class DrivableArea(Node):
                 cv2.fillPoly(occupancy_grid, [segment_array], 255)
                 time_of_frame = time.time()
 
+        # If the potholes are detected, put a mask of the potholes on the occupancy grid and mark the area as occupied
         if r_hole.boxes is not None:
             for segment in r_hole.boxes.xyxyn:
                 x_min, y_min, x_max, y_max = segment
@@ -147,34 +67,25 @@ class DrivableArea(Node):
                                     [x_min*image_width, y_max*image_height]], dtype=np.int32)
                 cv2.fillPoly(occupancy_grid, [vertices], color=(0, 0, 0))
 
+        # Calculate the buffer time
         buffer_area = np.sum(occupancy_grid)//255
         buffer_time = math.exp(-buffer_area/(image_width*image_height)-0.7)
         return occupancy_grid, buffer_time, time_of_frame
         
     def listener_callback(self, msg):
         
-        #converts Image message to cv2 type
+        # Convert the ROS message to a cv2 image
         frame = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+
+        # Resize the image to 720x1280
         frame = cv2.resize(frame, (1280, 720))
 
-        # runs YOLO predictions on the cv2 image
-        # results = lane_model.predict(frame, conf=0.25)
-        # masks = results[0].masks.xy
-        # print(masks)
-        
-        # grid = np.zeros((frame.shape[0], frame.shape[1]))
-        # print(grid.shape)
-    
-        # for mask in masks:
-        #     mask = np.array(mask, dtype=np.int32)
-        #     instance_mask = np.ones((frame.shape[0], frame.shape[1]))
-        #     cv2.fillPoly(instance_mask, [mask], 0)
-        #     np.logical_or(grid, instance_mask)
-            
-        # occupancy_grid_display = grid.astype(np.int8) * 255
+        # Get the occupancy grid
         occupancy_grid_display, buffer_time, time_of_frame = self.get_occupancy_grid(frame)
         total = np.sum(occupancy_grid_display)
         curr_time = time.time()
+
+        # If the occupancy grid is undetectable, display the previous frame
         if total == 0:
             if curr_time - time_of_frame < buffer_time:
                 occupancy_grid_display = memory_buffer
@@ -183,50 +94,63 @@ class DrivableArea(Node):
         else:
             memory_buffer = occupancy_grid_display
 
+        # Get the bird's eye view of the occupancy grid
         transformed_image, bottomLeft, bottomRight, topRight, topLeft, maxWidth, maxHeight = getBirdView(occupancy_grid_display, self.zed)
 
         maxHeight = int(maxHeight)
         maxWidth = int(maxWidth)
 
+
+        # Create a mask to remove the area outside the drivable area
         mask = np.full((maxHeight, maxWidth), -1, dtype=np.int8)
         pts =  np.array([bottomLeft, [bottomRight[0] - 27, bottomRight[1]], [topRight[0] - 65, topRight[1]], topLeft])
         pts = pts.astype(np.int32)  # convert points to int32
         pts = pts.reshape((-1, 1, 2))  # reshape points
         cv2.fillPoly(mask, [pts], True, 0)
 
+        # Apply the mask to the occupancy grid
         indicies = np.where(mask == -1)
         transformed_image[indicies] = -1
 
+        # Add a negative border to the occupancy grid
         add_neg = np.full((transformed_image.shape[0], 66), -1, dtype=np.int8)
 
+        # Concatenate the negative border to the occupancy grid
         transformed_image = np.concatenate((add_neg, transformed_image), axis=1)
         
+        # Convert the occupancy grid to a binary grid
         transformed_image = np.where(transformed_image==255, 1, transformed_image)
         transformed_image = np.where((transformed_image != 0) & (transformed_image != 1) & (transformed_image != -1), -1, transformed_image)
 
         new_size = (int(transformed_image.shape[1] * self.scale_factor), int(transformed_image.shape[0] * self.scale_factor))
         resized_image = cv2.resize(transformed_image, new_size, interpolation = cv2.INTER_NEAREST_EXACT)
 
+        # Create a robot occupancy grid to display the robot's position
         rob_arr = np.full((22, 169), -1, dtype=np.int8)
         rob_arr[10][85] = 2
 
+        # Concatenate the robot occupancy grid to the occupancy grid
         combined_arr = np.vstack((resized_image, rob_arr))
+        self.send_occupancy_grid(combined_arr)
 
+    def send_occupancy_grid(self, array):
         grid = OccupancyGrid()
         grid.header = Header()
+        grid.header.stamp = self.get_clock().now().to_msg()
         grid.header.frame_id = 'map'
         grid.info = MapMetaData()
-        grid.info.resolution = self.desired_size
-        grid.info.width = combined_arr.shape[1]
-        grid.info.height = combined_arr.shape[0]
-        grid.info.origin.position.x = float(34)
-        grid.info.origin.position.y = float(85)
+        grid.info.resolution = 0.05
+        grid.info.width = 169
+        grid.info.height = 22
+        grid.info.origin.position.x = 34.0
+        grid.info.origin.position.y = 85.0
+        grid.info.origin.position.z = 0.0
 
-        grid.data = Array('b', combined_arr.ravel().astype(np.int8))
+        grid.data = Array('b', array.ravel().astype(np.int8))
 
         self.publisher.publish(grid)
-
         self.get_logger().info('Publishing occupancy grid')
+
 
 def main(args=None):
     rclpy.init(args=args)
